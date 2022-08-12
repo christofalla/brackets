b8b14e6bdb408b8745614b0020e9829e6a9d08eb
christofalla <kristina111143@gmail.com>
2022-08-12 14:02:37 +0300

    feat: end code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+
+    let array = bracketsConfig.map(elem => elem.join(""));
+    for (let i = 0 ; i < array.length; i++) {
+
+         if (str.includes(array[i])) {
+
+          str = str.replace(array[i], "");
+           i = -1;
+
+         }
+    }
+    return (str) ? false : true;
+
+  };
